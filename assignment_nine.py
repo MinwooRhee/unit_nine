eb9862b4ace9330a677f1cc47ab442761a3cdd20
Min Woo <minwoo@iMac5.local>
2018-11-29 09:57:41 -0500

    finished program, need docstring

--- assignment_nine.py
+++ assignment_nine.py
@@ -2,30 +2,55 @@
 import deck
 
 
+def deal_cards(deck1):
+
+    player1_cards = []
+    player2_cards = []
+
+    for x in range(5):
+        player1_cards.append(deck1.draw_a_card())
+        player2_cards.append(deck1.draw_a_card())
+
+    return player1_cards, player2_cards
+
+
+def compare_cards(card1, card2):
+
+    if card1.compared_to(card2) == card1:
+        return "Player One"
+    else:
+        return "Player Two"
+
+
 def main():
 
     deck1 = deck.Deck()
 
-    for card in deck1.deck_of_cards:
-
-        print(card)
-
     deck1.shuffle()
-    print(" ")
 
-    for card in deck1.deck_of_cards:
+    player1_score = 0
+    player2_score = 0
 
-        print(card)
+    player1_cards, player2_cards = deal_cards(deck1)
 
-    playerCards = []
-    dealerCards = []
+    for x in range(0, 5):
 
-    for x in range(5):
-        playerCards.append(deck1.draw_a_card())
-        dealerCards.append(deck1.draw_a_card())
+        print("Player one has", player1_cards[x])
+        print("Player two has", player2_cards[x])
 
-    print(playerCards)
-    print(dealerCards)
+        if compare_cards(player1_cards[x], player2_cards[x]) == "Player One":
+            player1_score += 1
+            print("Player One wins this round!")
+        else:
+            player2_score += 1
+            print("Player Two wins this round!")
+
+        print(" ")
+
+    if player1_score > player2_score:
+        print("Player One wins the game!")
+    else:
+        print("Player Two wins the game!")
 
 
 main()
